9405681b00cfb2e481618153c8a1d856ac982661
umamiMike <michael.w.chastain@gmail.com>
2015-11-30 15:04:23 -0800

    express working

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-var app = require('http').createServer(handler),
-     io = require('socket.io').listen(app),
+var express = require('express'),
+    app = express();
+    server = require('http').createServer(app),
+     io = require('socket.io')(server),
      fs = require('fs'),
-   five = require('johnny-five');
+   five = require('johnny-five'),
+   port = process.env.PORT || 3000;
 
-app.listen(8080);
+//app.listen(8090);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-board = new five.Board();
-
-board.on("ready", function() {
-  led = new five.Led(13);
-
-  io.sockets.on('connection', function (socket) {
-    socket.on('click', function () {
-      led.toggle();
-    });
-  });
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+
+    // function (err, data) {
+    //   if (err) {
+    //     res.writeHead(500);
+    //     return res.end('Error loading index.html');
+    //   }
+    //
+    //   res.writeHead(200);
+    //   res.end(data);
+    //};
+});
+
+
+
+//board = new five.Board();
+
+// board.on("ready", function() {
+//   led = new five.Led(13);
+//
+//   io.on('connection', function (socket) {
+//
+//     socket.on('click', function () {
+//       led.toggle();
+//     });
+//
+//     socket.on('balls', function (){
+//
+//     });
+//   });
+// });
