f35384969c30ab5300772ba57a3c8b5ec5f74a1c
Mike Chastain <michaelwchastain@gmail.com>
2015-12-04 15:34:47 -0800

    updates from demo day

--- script.js
+++ script.js
@@ -22,25 +22,81 @@ app.get('/',function(req,res){
 });
 //still need to understand how the express route works
 app.get('/led-on',function(req,res){
-    // var socket = io.connect('http://10.0.0.199:3000');
-    // socket.emit('click');
+
 res.send('you are at led-on');
-//app.locals.led.toggle();
+
+});
+app.get('/green',function(req,res){
+
+res.send('you are at green');
+redled.brightness(0);
+blueled.brightness(0);
+greenled.brightness(255);
+
+});
+app.get('/blink',function(req,res){
+
+res.send('you are at green');
+redled.blink();
+blueled.blink();
+greenled.blink();
+
+});
+
+app.get('/strobe',function(req,res){
+
+res.send('you are at green');
+redled.strobe(400);
+blueled.strobe(500);
+greenled.strobe(600);
+
 });
 
 board = new five.Board();
 
 board.on("ready", function() {
   led = new five.Led(11);
+redled = new five.Led(5);
+greenled = new five.Led(6);
+blueled = new five.Led(9);
+
+  // rgbLed = new five.Led.RGB({
+  //   pins: {
+  //     red: 6,
+  //     green: 5,
+  //     blue: 3
+  //   }
+  // });
 
   io.on('connection', function (socket) {
-
+var amt = 1000;
     socket.on('click', function () {
       led.toggle();
     });
 
     socket.on('balls', function (){
         led.brightness(getRandom(0,255));
+    });
+
+    socket.on('red', function (data){
+      //  rbgled.red(data);
+      redled.fade(data,amt);
+    });
+    socket.on('bluegreen', function (data){
+      //  rbgled.red(data);
+      blueled.fade(data,amt);
+      greenled.fade(data,amt);
+    });
+    socket.on('green', function (data){
+      greenled.fade(data,amt);
+    });
+    socket.on('blue', function (data){
+      blueled.fade(data,amt);
+    });
+    socket.on('brightness', function (data){
+      blueled.brightness(data);
+      greenled.brightness(data);
+      redled.brightness(data);
     });
   });
 });
